2e4145f7018ed2305eb4b28982f0caf94d9d5ad4
Danilo <danilo.cavallini.01@gmail.com>
2024-07-15 15:34:15 +0200

    oprobot and incinerator

--- Sprint1/Sprint1_Progetto/Arch_wastestorage.py
+++ Sprint1/Sprint1_Progetto/Arch_wastestorage.py
@@ -25,8 +25,10 @@ with Diagram('wastestorageArch', show=False, outformat='png', graph_attr=graphat
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
 ### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
-     with Cluster('ctx_wis', graph_attr=nodeattr):
-          incinerator=Custom('incinerator','./qakicons/symActorSmall.png')
+     with Cluster('ctx_wastestorage', graph_attr=nodeattr):
+          waste_storage=Custom('waste_storage','./qakicons/symActorSmall.png')
+     with Cluster('ctx_oprobot', graph_attr=nodeattr):
           op_robot=Custom('op_robot(ext)','./qakicons/externalQActor.png')
-          wis=Custom('wis(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctx_mock_external_entity', graph_attr=nodeattr):
+          mock_external_entity=Custom('mock_external_entity(ext)','./qakicons/externalQActor.png')
 diag
--- Sprint1/Sprint1_Progetto/wastestorage.pl
+++ Sprint1/Sprint1_Progetto/wastestorage.pl
@@ -15,8 +15,10 @@ dispatch( deposit_ash, deposit_ash(X) ).
 request( empty_ash, empty_ash(X) ).
 request( moverobot, moverobot(TARGETX,TARGETY) ).
 %====================================================================================
-context(ctx_wis, "localhost",  "TCP", "8014").
- qactor( op_robot, ctx_wis, "external").
-  qactor( wis, ctx_wis, "external").
-  qactor( incinerator, ctx_wis, "it.unibo.incinerator.Incinerator").
- static(incinerator).
+context(ctx_wastestorage, "localhost",  "TCP", "8016").
+context(ctx_oprobot, "127.0.0.1",  "TCP", "8017").
+context(ctx_mock_external_entity, "127.0.0.2",  "TCP", "8018").
+ qactor( op_robot, ctx_oprobot, "external").
+  qactor( mock_external_entity, ctx_mock_external_entity, "external").
+  qactor( waste_storage, ctx_wastestorage, "it.unibo.waste_storage.Waste_storage").
+ static(waste_storage).
