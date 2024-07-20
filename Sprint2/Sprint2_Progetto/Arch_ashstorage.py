39028341da53acd0eb7cb14b855750b67093544e
Alessandro Piombini <apiombo6@gmail.com>
2024-07-20 16:38:11 +0200

    update

---
+++ Sprint2/Sprint2_Progetto/Arch_ashstorage.py
@@ -0,0 +1,32 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+evattr = {
+    'color': 'darkgreen',
+    'style': 'dotted'
+}
+with Diagram('ashstorageArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctx_wis', graph_attr=nodeattr):
+          ash_storage=Custom('ash_storage','./qakicons/symActorSmall.png')
+     with Cluster('ctx_external_entity', graph_attr=nodeattr):
+          external_entity=Custom('external_entity(ext)','./qakicons/externalQActor.png')
+diag
