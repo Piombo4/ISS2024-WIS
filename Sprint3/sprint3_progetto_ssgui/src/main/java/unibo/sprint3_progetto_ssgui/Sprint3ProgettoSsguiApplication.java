940d04ffee2524917b3a19d6cc6421ddd8494f36
Piombo4 <apiombo6@gmail.com>
2024-07-21 16:26:43 +0200

    update

---
+++ Sprint3/sprint3_progetto_ssgui/src/main/java/unibo/sprint3_progetto_ssgui/Sprint3ProgettoSsguiApplication.java
@@ -0,0 +1,13 @@
+package unibo.sprint3_progetto_ssgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint3ProgettoSsguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint3ProgettoSsguiApplication.class, args);
+	}
+
+}
---
+++ Sprint3/sprint3_progetto_ssgui/src/test/java/unibo/sprint3_progetto_ssgui/Sprint3ProgettoSsguiApplicationTests.java
@@ -0,0 +1,13 @@
+package unibo.sprint3_progetto_ssgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint3ProgettoSsguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
